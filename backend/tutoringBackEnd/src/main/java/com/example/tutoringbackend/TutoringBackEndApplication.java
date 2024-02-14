0c2fa031bf60fd0f864dc4b497b4f9f274ed42c1
usman khan <muak1@macbook-pro-3.ucalgary.ca>
2024-02-14 09:04:48 -0700

    modified backend

--- backend/tutoringBackEnd/src/main/java/com/example/tutoringbackend/TutoringBackEndApplication.java
+++ backend/tutoringBackEnd/src/main/java/com/example/tutoringbackend/TutoringBackEndApplication.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package com.example.tutoringbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class TutoringBackEndApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(TutoringBackEndApplication.class, args);
     }
 
 }
--- backend/tutoringBackEnd/src/test/java/com/example/tutoringbackend/TutoringBackEndApplicationTests.java
+++ backend/tutoringBackEnd/src/test/java/com/example/tutoringbackend/TutoringBackEndApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.example.tutoringbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class TutoringBackEndApplicationTests {
 
     @Test
     void contextLoads() {
