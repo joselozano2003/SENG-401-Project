35d0d4ceb302f338fcfd12121a552b405b92942a
Angelo Troncone <angelotroncone@gmail.com>
2024-02-19 13:21:44 -0700

    Backend runs successfully when pgadmin is open

--- backend/src/main/java/com/example/tutor/TutorController.java
+++ backend/src/main/java/com/example/tutor/TutorController.java
@@ -1,18 +1,14 @@
 package com.example.tutor;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
 @RequestMapping("api/v1/tutor")
-public class TutorController {
+class TutorController {
+
     private final TutorService tutorService;
 
     public TutorController(TutorService tutorService) {
@@ -20,24 +16,7 @@ public class TutorController {
     }
 
     @GetMapping
-    public String getMethodName(@RequestParam String param) {
-        return new String("List of tutors");
+    public String getMethodName() {
+        return new String("Hello World");
     }
-
-    @PostMapping
-    public String postMethodName(@RequestBody String entity) {
-        return tutorService.addTutor(entity);
-    }
-
-    @DeleteMapping
-    public String deleteMethodName(@RequestParam String param) {
-        return tutorService.deleteTutor(param);
-    }
-
-    @PutMapping(path = "{id}")
-    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
-        return tutorService.updateTutor(id, entity);
-    }
-
-
 }
